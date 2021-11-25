751e1c988efd36d0b31f6cc0b4a044f757eaf791
Van Hung <vanhungnguyenbku@gmail.com>
2021-11-25 15:39:14 +0700

    .

--- src/views/app-views/homepage/video/index.js
+++ src/views/app-views/homepage/video/index.js
@@ -1,7 +1,168 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Table, Select, Input, Button, Tooltip } from 'antd'
+import VideoData from 'assets/data/video.data.json'
+import {
+	DeleteOutlined,
+	SearchOutlined,
+	PlusCircleOutlined,
+	EditOutlined,
+} from '@ant-design/icons'
+
+import Flex from 'components/shared-components/Flex'
+import utils from 'utils'
+
+const { Option } = Select
+
+const types = ['Active', 'Not active']
 
 const Video = () => {
-	return <div>Video</div>
+	const [list, setList] = useState(VideoData)
+	const [selectedRows, setSelectedRows] = useState([])
+	const [selectedRowKeys, setSelectedRowKeys] = useState([])
+
+	const onSearch = (e) => {
+		const value = e.currentTarget.value
+		const searchArray = e.currentTarget.value ? list : VideoData
+		const data = utils.wildCardSearch(searchArray, value)
+		setList(data)
+		setSelectedRowKeys([])
+	}
+
+	const handleShowType = (value) => {
+		if (value !== 'Active') {
+			const key = 'active'
+			const data = utils.filterArray(VideoData, key, value)
+			setList(data)
+		} else {
+			setList(VideoData)
+		}
+	}
+
+	const addKey = () => {}
+	const deleteKey = () => {}
+
+	const rowSelection = {
+		onChange: (key, rows) => {
+			setSelectedRows(rows)
+			setSelectedRowKeys(key)
+		},
+	}
+
+	const tableColumns = [
+		{
+			title: 'Image',
+			dataIndex: 'image',
+		},
+		{
+			title: 'Title',
+			dataIndex: 'title',
+			sorter: (a, b) => utils.antdTableSorter(a, b, 'title'),
+		},
+		{
+			title: 'Url',
+			dataIndex: 'url',
+			sorter: (a, b) => utils.antdTableSorter(a, b, 'url'),
+		},
+		{
+			title: 'Time',
+			dataIndex: 'time',
+		},
+		{
+			title: 'Active',
+			dataIndex: 'active',
+			sorter: (a, b) => utils.antdTableSorter(a, b, 'active'),
+		},
+		{
+			title: 'Action',
+			dataIndex: 'actions',
+			render: (_, elm) => (
+				<div className='text-right d-flex justify-content-end'>
+					<Tooltip title='Edit'>
+						<Button
+							type='primary'
+							className='mr-2'
+							icon={<EditOutlined />}
+							size='small'
+						/>
+					</Tooltip>
+					<Tooltip title='Delete'>
+						<Button danger icon={<DeleteOutlined />} size='small' />
+					</Tooltip>
+				</div>
+			),
+		},
+	]
+
+	return (
+		<Card>
+			<Flex
+				alignItems='center'
+				justifyContent='between'
+				mobileFlex={false}
+			>
+				<Flex className='mb-1' mobileFlex={false}>
+					<div className='mr-md-3 mb-3'>
+						<Input
+							placeholder='Search'
+							prefix={<SearchOutlined />}
+							onChange={(e) => onSearch(e)}
+						/>
+					</div>
+					<div className='mb-3'>
+						<Select
+							defaultValue='Active'
+							className='w-100'
+							style={{ minWidth: 180 }}
+							onChange={handleShowType}
+							placeholder='Active'
+						>
+							<Option value='Active'>Active</Option>
+							{types.map((elm, i) => (
+								<Option key={i} value={elm}>
+									{elm}
+								</Option>
+							))}
+						</Select>
+					</div>
+				</Flex>
+				<Flex mobileFlex={false} justifyContent='end'>
+					<div className='mr-1'>
+						<Button
+							onClick={addKey}
+							type='primary'
+							icon={<PlusCircleOutlined />}
+							block
+						>
+							Add key
+						</Button>
+					</div>
+					<div>
+						<Button
+							onClick={deleteKey}
+							type='danger'
+							icon={<DeleteOutlined />}
+							block
+							disabled={true}
+						>
+							Delete key
+						</Button>
+					</div>
+				</Flex>
+			</Flex>
+			<div className='table-responsive'>
+				<Table
+					columns={tableColumns}
+					dataSource={list}
+					rowKey='name'
+					rowSelection={{
+						type: 'checkbox',
+						...rowSelection,
+					}}
+					bordered={true}
+				/>
+			</div>
+		</Card>
+	)
 }
 
 export default Video
